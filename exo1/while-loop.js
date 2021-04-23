3e3d52278968f3d9fcce5c8dc47805ea7fe2fa6a
goueguy <jlagoueguy@gmail.com>
2021-04-23 10:36:11 +0000

    do while loop practice

---
+++ exo1/while-loop.js
@@ -0,0 +1,14 @@
+//LOOPS
+//Repeatedly run a block of code while condition is true 
+
+//while loop
+
+
+//turn off autosave
+
+
+let amount = 10;
+while(amount>0){
+    console.log("I HAVE "+ amount + " dollars and i m going to the mall");
+    amount--;
+}
